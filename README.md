efdf86d149fedc5c9383c07ed1f6a6696fc24a1c
Bala Paranj <bparanj@onekingslane.com>
2016-05-09 14:55:55 -0700

    before - working version

--- README.md
+++ README.md
@@ -11,3 +11,28 @@ class User < ApplicationRecord
   
 end
 
+rails db:migrate
+
+rails s
+
+Create an user with an image.
+
+Version thumb doesn't exist!
+
+Add:
+
+version :thumb do
+  resize_to_fill(100, 100)
+end
+
+to avatar_uploader.rb
+
+undefined method `resize_to_fill' for Class
+
+
+Uncomment:
+
+  include CarrierWave::RMagick
+  
+in avatar_uploader.rb.
+
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -14,6 +14,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    resize_to_fill(100, 100)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
